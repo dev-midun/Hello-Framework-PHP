2da687ec4cd7bcc01eb83b298552c425943f9551
Romadan Saputra <42397721+dev-dmadan@users.noreply.github.com>
2020-04-28 23:24:11 +0700

    Update Home.controller.php

--- app/controller/Home.controller.php
+++ app/controller/Home.controller.php
@@ -2,10 +2,13 @@
 Defined('BASE_PATH') or die(ACCESS_DENIED);
 
 class Home extends Controller {
-
+    
+    private $isCanAccess;
+    
     public function __construct() {
         $this->auth();
         $this->auth->alreadyLogin();
+        $this->isCanAccess = $this->auth->isCanAccess('Home');
         $this->model('Home');
     }
 
@@ -14,16 +17,20 @@ class Home extends Controller {
      * Default Main Method
      */
     public function index() {
-        // $config = (object)array(
-        //     'js' => array(
-        //         (object)array(
-        //             'src' => ADMIN_LTE. 'dist/js/pages/dashboard.js'
-        //         ),
-        //         (object)array(
-        //             'src' => ADMIN_LTE. 'dist/js/demo.js'
-        //         )
-        //     )
-        // );
+        if(!$this->isCanAccess || !$this->isCanAccess->isCanRead) {
+            $this->responseError(403, 'Access Denied');
+        }
+
+        $config = (object)array(
+            'js' => array(
+                (object)array(
+                    'src' => ADMIN_LTE. 'dist/js/pages/dashboard.js'
+                ),
+                (object)array(
+                    'src' => ADMIN_LTE. 'dist/js/demo.js'
+                )
+            )
+        );
 
         $this->template('HelloWorld/starter');
     }
